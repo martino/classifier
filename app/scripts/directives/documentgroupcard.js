be8a02f0b2cfa5b6f3c52599c0d48f30327ba68a
Martino Pizzol <martino.pizzol@gmail.com>
2015-02-01 16:47:24 +0100

    Added creation for document group

--- app/scripts/directives/documentgroupcard.js
+++ app/scripts/directives/documentgroupcard.js
@@ -7,14 +7,27 @@
  * # DocumentGroupCard
  */
 angular.module('classifierApp')
-  .directive('documentGroupCard', function () {
+  .directive('documentGroupCard', ['datatxt', function (datatxt) {
     return {
       templateUrl: 'views/directive-documentgroupcard.html',
       restrict: 'E',
       scope: {
         documentGroup: '='
       },
-      link: function postLink(scope, element, attrs) {
+      link: function (scope, element, attrs, ngModel) {
+
+        scope.showNewDGForm = false;
+        scope.newModel = {
+          name: ''
+        };
+        scope.createDocumentGroup = function () {
+          console.log('create', scope.newModel);
+          if (scope.newModel.name !== '')
+            datatxt.createNewDocumentGroup(scope.newModel.name);
+        };
+        scope.toggleForm = function () {
+          scope.showNewDGForm = !scope.showNewDGForm;
+        };
       }
     };
-  });
+  }]);
